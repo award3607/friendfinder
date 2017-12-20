89b35c3f654aa390b66cc3f8cb3307b887e1552f
Andrew Ward <andrew.rampant@gmail.com>
2017-12-19 22:24:45 -0600

    ready for heroku push

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var apiRoutes = require('./routing/apiRoutes.js');
 var htmlRoutes = require('./routing/htmlRoutes.js');
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 app.use(apiRoutes);
